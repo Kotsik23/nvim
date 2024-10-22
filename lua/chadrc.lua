1853a0b395d126c5f36b038e484d4104aa322a1c
olegsmushko <olegsmushko@gmail.com>
2024-10-22 21:42:12 +0300

    Terminal customizations

--- lua/chadrc.lua
+++ lua/chadrc.lua
@@ -1,18 +1,29 @@
--- This file needs to have same structure as nvconfig.lua 
+-- This file needs to have same structure as nvconfig.lua
 -- https://github.com/NvChad/ui/blob/v3.0/lua/nvconfig.lua
--- Please read that file to know all available options :( 
+-- Please read that file to know all available options :(
 
 ---@type ChadrcConfig
 local M = {}
 
 M.base46 = {
-	theme = "nightlamp",
-  transparency = true
+  theme = "nightlamp",
+  transparency = true,
 
-	-- hl_override = {
-	-- 	Comment = { italic = true },
-	-- 	["@comment"] = { italic = true },
-	-- },
+  hl_override = {
+    Comment = { italic = true },
+    ["@comment"] = { italic = true },
+  },
+}
+
+M.term = {
+  cmd = "fastfetch",
+  float = {
+    row = 0.05,
+    col = 0.10,
+    width = 0.8,
+    height = 0.7,
+    border = "single",
+  },
 }
 
 return M
