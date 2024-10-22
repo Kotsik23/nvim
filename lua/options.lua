8a478cd435dfc2f8b9c466bbdd90ec49fc239efe
olegsmushko <olegsmushko@gmail.com>
2024-10-22 20:34:41 +0300

    init commit

---
+++ lua/options.lua
@@ -0,0 +1,10 @@
+require "nvchad.options"
+
+-- add yours here!
+
+local o = vim.o
+o.so = 8
+o.number = true
+o.relativenumber = true
+o.clipboard="unnamedplus,unnamed"
+-- o.cursorlineopt ='both' -- to enable cursorline!
